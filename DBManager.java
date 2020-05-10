f5e410c08e00a5de447795978ee99de128468196
vincentjarnung <vincentjarnung98@gmail.com>
2020-05-10 12:48:54 +0200

    Lite java ändringar

--- DBManager.java
+++ DBManager.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.math.BigDecimal;
 import java.sql.*;
 import javax.swing.*;
 import java.util.*;
@@ -6,7 +7,17 @@ import org.sqlite.SQLiteConfig;
 
 public class DBManager {
    
-   public static int memID = 00000;
+   String memID = "";
+   long PNR = 0;
+   String first_name = "";
+   String last_name = "";
+   String email = "";
+   int phoneNumber = 0;
+   String adress = "";
+   int postalCode = 0;
+   String city = "";
+   String password = "";
+   String memshipID = "";
    
    public static Connection conn = null;
    // Sökväg till SQLite-databas. OBS! Ändra sökväg så att den pekar ut din databas
@@ -16,7 +27,7 @@ public class DBManager {
    
    public static void main(String[] args){
          // Kod för att skapa uppkoppling mot SQLite-dabatasen
-         String KEY_ON = "PRAGMA foreign_keys = on";
+         //String KEY_ON = "PRAGMA foreign_keys = on";
       try {
          Class.forName(DRIVER);
          SQLiteConfig config = new SQLiteConfig();  
@@ -28,12 +39,18 @@ public class DBManager {
          System.exit(0);
   
       }
-      menu();
+      DBManager men = new DBManager();
+      men.menu();
+      
+      
       
    }
-   public static void menu(){
-      while (true){
-         String[] options = {"Avsluta","Logga In","Våra Gym","Boka Pass","Bli Medlem"};
+   
+   public void menu(){
+	   System.out.println(getNumOfRows("Member"));
+	   
+	   while (true){
+		   String[] options = {"Avsluta","Logga In","Våra Gym","Boka Pass","Bli Medlem"};
          int x = JOptionPane.showOptionDialog(null, "Välkommen Till FitnessAB","Meny",
                                               JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, null);
          //Om man stänger rutan stängs programmet
@@ -44,7 +61,8 @@ public class DBManager {
          switch(x){
             case 4:
             //Bli Medlem
-            JOptionPane.showMessageDialog(null,(BliMedlem()));
+            	JOptionPane.showMessageDialog(null,(BliMedlem()));
+            	menu();
             break;
             case 3:
             //Boka pass
@@ -66,89 +84,60 @@ public class DBManager {
       }
    }
    
-   public static String BliMedlem(){
-      String sql = "INSERT INTO Members VALUES(?,?,?,?,?,?,?,?,?,?,?)";
-      String result = "";
+   public String BliMedlem(){
+      String sql = "INSERT INTO Member VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+      String result = ""; 
+      Boolean[] li = new Boolean[9];
+      Arrays.fill(li,Boolean.TRUE);
+      String[] ans = new String[9];
       
-      int PNR = 0;
-      String first_name = "";
-      String last_name = "";
-      String email = "";
-      int phoneNumber = 0;
-      String adress = "";
-      int postalCode = 0;
-      String city = "";
-      String password = "";
-      String memship = "";
+      System.out.println(li[0]);
       
-      JTextField field1 = new JTextField();
-      JTextField field2 = new JTextField();
-      JTextField field3 = new JTextField();
-      JTextField field4 = new JTextField();
-      JTextField field5 = new JTextField();
-      JTextField field6 = new JTextField();
-      JTextField field7 = new JTextField();
-      JTextField field8 = new JTextField();
-      JTextField field9 = new JTextField();
+      generateMemDialog(li, ans);  
       
-      Object[] message = {
-         "Personnummer ÅÅMMDD:", field1,
-         "Förnamn:", field2,
-         "Efternamn:", field3,
-         "Mejl:", field4,
-         "Telefonnummer:", field5,
-         "Adress:", field6,
-         "Postnummer:", field7,
-         "Stad:", field8,
-         "Lösenord:", field9,
-      };
-     
-      int option = JOptionPane.showConfirmDialog(null, message, "Skriv in dina uppgifer!", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-      if (option == JOptionPane.OK_OPTION){  
-         try {
-            PNR = Integer.parseInt(field1.getText());
-         }
-         catch(Exception e){
-            String t = "<html><font color=#ffffdd>Hello</font> world!";
-         }
-         first_name = field2.getText();
-         last_name = field3.getText();
-         email = field4.getText();
-         phoneNumber = Integer.parseInt(field5.getText());
-         adress = field6.getText();
-         postalCode = Integer.parseInt(field7.getText());
-         city = field8.getText();
-         password = field9.getText();
-      }
       String[] options = {"Gruppträning","Gymträning"};
       int x = JOptionPane.showOptionDialog(null, "Välj Medlemskap \n\nGymträning: \nDu får tillgång till gymmen och kan träna där fritt! \n\nGruppträning: \nDu får tillgång till gymmen och gruppträningar samt onlineträning! ","Välj Medlemskap",
                                               JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, JOptionPane.PLAIN_MESSAGE);
       if (x == JOptionPane.CLOSED_OPTION)
-         System.exit(0);   
+         menu();   
       if (x==0)
-         memship = "Gruppträning";
+         memshipID = "Gruppträning";
       if (x==1)
-         memship = "Gymträning";
+         memshipID = "Gymträning";
       
-      memID = 5;
+      int counter = getNumOfRows("Member");
+      
+      if (counter <10)
+          memID = "mem0000" + counter;
+       else if (counter <100)
+          memID = "mem000" + counter;
+       else if (counter <1000)
+          memID = "mem00" + counter;
+       else if (counter <10000)
+          memID = "mem0" + counter;
+       else
+    	  memID = "mem" + counter;
+      
       System.out.println(memID);
+   
       try {
             PreparedStatement pstmt = conn.prepareStatement(sql);
             System.out.println("Funkar kung");
             
             
             //Funkar allt ändrar jag frågetäcknena i sql-stringen(sql) till de som någon har matat in
-            pstmt.setInt(1, memID);
-            pstmt.setInt(2, PNR);
-            pstmt.setString(3, first_name);
-            pstmt.setString(4, last_name);
-            pstmt.setString(5, email);
-            pstmt.setInt(6, phoneNumber);
-            pstmt.setString(7, adress);
-            pstmt.setInt(8, postalCode);
-            pstmt.setString(9, city);
-            pstmt.setString(10, password);
-            pstmt.setString(11, memship);
+            pstmt.setObject(1, memID);
+            pstmt.setObject(2, new BigDecimal(PNR));
+            pstmt.setObject(3, first_name);
+            pstmt.setObject(4, last_name);
+            pstmt.setObject(5, email);
+            pstmt.setObject(6, phoneNumber);
+            pstmt.setObject(7, adress);
+            pstmt.setObject(8, postalCode);
+            pstmt.setObject(9, city);
+            pstmt.setObject(10, memshipID);
+            pstmt.setObject(11, password);
+            
             
             pstmt.executeUpdate();
             //Det lyckades och då sätts stringen(result) som ska returnas till ett bekräftande
@@ -162,4 +151,125 @@ public class DBManager {
         
       return result;
    }       
+    
+   public void generateMemDialog(Boolean[] lis, String[] ans){
+
+      
+      JTextField[] fields = new JTextField[]{
+      new JTextField(),
+      new JTextField(),
+      new JTextField(),
+      new JTextField(),
+      new JTextField(),
+      new JTextField(),
+      new JTextField(),
+      new JTextField(),
+      new JTextField()};
+
+      Boolean checker = true;
+      
+      Object[] message = {
+         "Personnummer - ÅÅÅÅMMDDNNNN:", fields[0],
+         "Förnamn:", fields[1],
+         "Efternamn:", fields[2],
+         "Mejl:", fields[3],
+         "Telefonnummer:", fields[4],
+         "Adress:", fields[5],
+         "Postnummer:", fields[6],
+         "Stad:", fields[7],
+         "Lösenord:", fields[8],
+      };
+      
+      
+      for(int n = 0, i = 0; i < lis.length; i ++, n += 2){
+         if (lis[i] == false) 
+            message[n] = "<html><font color=#FF0000>" + message[n] + "</font>";
+         else{
+            fields[i].setText(ans[i]); 
+         }
+      }
+      
+ 
+      int option = JOptionPane.showConfirmDialog(null, message,"Skriv in dina uppgifer!", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+      if (option == JOptionPane.CLOSED_OPTION)
+          menu();  
+      if (option == JOptionPane.CANCEL_OPTION){
+         menu();
+      }
+      Boolean[] nli = new Boolean[9];
+      if (option == JOptionPane.OK_OPTION){  
+         if (fields[0].getText() != null && fields[0].getText().length() == 12 && fields[0].getText().matches("[0-9]+")) {
+            ans[0] = fields[0].getText();
+            PNR = Long.parseLong(fields[0].getText());
+            nli[0] = true;
+            
+         }else{ans[0] = ""; nli[0] = false; checker = false;}
+         if (fields[1].getText() != null) {
+            ans[1] = fields[1].getText();
+            first_name = fields[1].getText();
+            nli[1] = true;
+         }else{ans[1] = ""; nli[1] = false; checker = false;}
+         if (fields[2].getText() != null) {
+            ans[2] = fields[2].getText();
+            last_name = fields[2].getText();
+            nli[2] = true;
+         }else{ans[2] = ""; nli[2] = false; checker = false;}
+         if (fields[3].getText() != null){
+            ans[3] = fields[3].getText();
+            email = fields[3].getText();
+            nli[3] = true;
+         }else{ans[3] = ""; nli[3] = false; checker = false;}
+         if (fields[4].getText() != null && fields[4].getText().matches("[0-9]+")){
+            ans[4] = fields[4].getText();
+            phoneNumber = Integer.parseInt(fields[4].getText());
+            nli[4] = true;
+         }else{ans[4] = ""; nli[4] = false; checker = false;}
+         if (fields[5].getText() != null){
+            ans[5] = fields[5].getText();
+            adress = fields[5].getText();
+            nli[5] = true;
+         }else{ans[5] = ""; nli[5] = false; checker = false;}
+         if (fields[6].getText() != null && fields[6].getText().matches("[0-9]+")){
+            ans[6] = fields[6].getText();
+            postalCode = Integer.parseInt(fields[6].getText());
+            nli[6] = true;
+         }else{ans[6] = ""; nli[6] = false; checker = false;}
+         if (fields[7].getText() != null) {
+            ans[7] = fields[7].getText();
+            city = fields[7].getText();
+            nli[7] = true;
+         }else{ans[7] = ""; nli[7] = false; checker = false;}
+         if (fields[8].getText() != null) {
+            ans[8] = fields[8].getText();
+            password = fields[8].getText();
+            nli[8] = true;
+         }else{ans[8] = ""; nli[8] = false; checker = false;}
+
+      }
+      
+     
+      if (checker == false)
+         generateMemDialog(nli, ans);
+   }
+   
+   
+   public int getNumOfRows(String table) {
+	   String sql = "select count(*) from "+ table;
+	   int result = 0;
+	   
+	   try {
+           Statement stmt  = conn.createStatement();
+           ResultSet rs    = stmt.executeQuery(sql);
+           result = rs.getInt(1);
+       
+	   }catch (SQLException e) {
+		   System.out.println("Funkar ej");
+		   System.out.println(e.getMessage());
+	   }
+
+	   return result;
+   }
 }
+
+	
+
