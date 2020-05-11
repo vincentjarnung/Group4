c1aab5404b2251df6f435b5a2d446635c5bd7b00
vincentjarnung <vincentjarnung98@gmail.com>
2020-05-11 13:19:37 +0200

    Fixat till lite kod

--- DBManager.java
+++ DBManager.java
@@ -1,14 +1,12 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.io.*;
-import java.math.BigDecimal;
 import java.sql.*;
 import javax.swing.*;
 import java.util.*;
 import org.sqlite.SQLiteConfig;
 
 public class DBManager {
-   
+
    String memID = "";
    long PNR = 0;
    String first_name = "";
@@ -20,46 +18,45 @@ public class DBManager {
    String city = "";
    String password = "";
    String memshipID = "";
-   
+
    public static Connection conn = null;
    // Sökväg till SQLite-databas. OBS! Ändra sökväg så att den pekar ut din databas
-   public static final String DB_URL = "jdbc:sqlite:/Users/vincent/Desktop/Group4Java/FitnessAB.sqlite";  
+   public static final String DB_URL = "jdbc:sqlite:/Users/vincent/Desktop/Group4Java/FitnessAB.sqlite";
    // Namnet på den driver som används av java för att prata med SQLite
-   public static final String DRIVER = "org.sqlite.JDBC";  
-   
+   public static final String DRIVER = "org.sqlite.JDBC";
+
    public static void main(String[] args){
          // Kod för att skapa uppkoppling mot SQLite-dabatasen
          //String KEY_ON = "PRAGMA foreign_keys = on";
       try {
          Class.forName(DRIVER);
-         SQLiteConfig config = new SQLiteConfig();  
+         SQLiteConfig config = new SQLiteConfig();
          config.enforceForeignKeys(true);// Denna kodrad ser till att sätta databasen i ett läge där den ger felmeddelande ifall man bryter mot någon främmande-nyckel-regel
          conn = DriverManager.getConnection(DB_URL,config.toProperties());
          }catch (Exception e) {
             // Om java-progammet inte lyckas koppla upp sig mot databasen (t ex om fel sökväg eller om driver inte hittas) så kommer ett felmeddelande skrivas ut
          System.out.println( e.toString() );
          System.exit(0);
-  
+
       }
       DBManager men = new DBManager();
       men.menu();
-      
-      
-      
+
+
+
    }
-   
+
    public void menu(){
-	   System.out.println(getNumOfRows("Member"));
 	   
 	   while (true){
-		   String[] options = {"Avsluta","Logga In","Våra Gym","Boka Pass","Bli Medlem"};
+		 String[] options = {"Avsluta","Logga In","Våra Gym","Boka Pass","Bli Medlem"};
          int x = JOptionPane.showOptionDialog(null, "Välkommen Till FitnessAB","Meny",
                                               JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, null);
          //Om man stänger rutan stängs programmet
          if (x == JOptionPane.CLOSED_OPTION){
                System.exit(0);
          }
-      
+
          switch(x){
             case 4:
             //Bli Medlem
@@ -68,12 +65,12 @@ public class DBManager {
             break;
             case 3:
             //Boka pass
-         
-            break;  
+
+            break;
             case 2:
             //Våra Gym
-            
-            break;   
+
+            break;
             case 1:
             //Logga In
             	JOptionPane.showMessageDialog(null,(LoggaIn()));
@@ -82,33 +79,32 @@ public class DBManager {
             case 0:
             //Avsluta
                System.exit(0);
-         break;    
+         break;
          }
       }
    }
-   
+
    public String BliMedlem(){
-      String sql = "INSERT INTO Member VALUES(?,?,?,?,?,?,?,?,?,?,?)";
-      String result = ""; 
+      String result = "";
       Boolean[] li = new Boolean[9];
       Arrays.fill(li,Boolean.TRUE);
       String[] ans = new String[9];
-      
+
       System.out.println(li[0]);
-      generateMemDialog(li, ans);  
-      
+      generateMemDialog(li, ans);
+
       String[] options = {"Gruppträning","Gymträning"};
       int x = JOptionPane.showOptionDialog(null, "Välj Medlemskap \n\nGymträning: \nDu får tillgång till gymmen och kan träna där fritt! \n\nGruppträning: \nDu får tillgång till gymmen och gruppträningar samt onlineträning! ","Välj Medlemskap",
                                               JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, JOptionPane.PLAIN_MESSAGE);
       if (x == JOptionPane.CLOSED_OPTION)
-         menu();   
+         menu();
       if (x==0)
          memshipID = "memship02";
       if (x==1)
          memshipID = "memship01";
-      
+
       int counter = getNumOfRows("Member");
-      
+
       if (counter <10)
           memID = "mem0000" + counter;
        else if (counter <100)
@@ -119,57 +115,34 @@ public class DBManager {
           memID = "mem0" + counter;
        else
     	  memID = "mem" + counter;
-      
+
       System.out.println(memID);
-   
-      try {
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            System.out.println("Funkar kung");
-            
-            //Funkar allt ändrar jag frågetäcknena i sql-stringen(sql) till de som någon har matat in
-            pstmt.setObject(1, memID);
-            pstmt.setObject(2, new BigDecimal(PNR));
-            pstmt.setObject(3, first_name);
-            pstmt.setObject(4, last_name);
-            pstmt.setObject(5, email);
-            pstmt.setObject(6, phoneNumber);
-            pstmt.setObject(7, adress);
-            pstmt.setObject(8, postalCode);
-            pstmt.setObject(9, city);
-            pstmt.setObject(10, password);
-            pstmt.setObject(11, memshipID);
-            
-            
-            
-            pstmt.executeUpdate();
-            //Det lyckades och då sätts stringen(result) som ska returnas till ett bekräftande
-            result = first_name + " " + last_name + " är tillagd.";           
-      }
-      catch (SQLException e){
-            //Det misslyckades och då sätts stringen(result) till felmeddelandet som kommer från sql
-            System.out.println(e.toString()); 
-            result = e.toString();
-      } 
-        
+      Object[] memVal = {memID , PNR, first_name, last_name, email, phoneNumber, adress, postalCode, city, password, memshipID};
+      
+      if (insertToDB("Member",memVal))
+    	  result = first_name + " " + last_name + " är tillagd.";
+      else
+    	  result = "Där uppstod ett problem med databasen försök igen eller kontakta kundservice.";
+      
       return result;
-   }       
+   }
 
    public String LoggaIn() {
 	   String sql = "SELECT mail,password FROM Member";
-	   String result = ""; 
+	   String result = "";
 	   Boolean wrong = false;
 	   Boolean checker = false;
 	   String[] results = new String[2];
-	   
-	   
-	   
+
+
+
 	   while(true) {
 		   results = generateLogInDialog(wrong,results);
-		   
+
 		   try {
 	           Statement stmt  = conn.createStatement();
 	           ResultSet rs    = stmt.executeQuery(sql);
-	           
+
 	           while(rs.next()) {
 	        	   System.out.println(rs.getString("mail") + " : " + results[0]+ "\n" + rs.getString("password") + " : " + results[1]);
 	        	   if ( results[0].equals(rs.getString("mail")) && results[1].equals(rs.getString("password")) ) {
@@ -178,14 +151,14 @@ public class DBManager {
 	        	   }
 	           }
 	           System.out.println(checker);
-	           
+
 	           if (checker) {
 	        	   result = "Välkommen";
 	        	   break;
 	           }else {
 	        	   wrong = true;
 	           }
-	       
+
 		   }catch (SQLException e) {
 			   System.out.println("Funkar ej");
 			   System.out.println(e.getMessage());
@@ -193,10 +166,10 @@ public class DBManager {
 	   }
 	   return result;
    }
-   
+
    public void generateMemDialog(Boolean[] lis, String[] ans){
 
-      
+
       JTextField[] fields = new JTextField[]{
       new JTextField(),
       new JTextField(),
@@ -209,7 +182,7 @@ public class DBManager {
       new JTextField()};
 
       Boolean checker = true;
-      
+
       Object[] message = {
          "Personnummer - ÅÅÅÅMMDDNNNN:", fields[0],
          "Förnamn:", fields[1],
@@ -221,30 +194,30 @@ public class DBManager {
          "Stad:", fields[7],
          "Lösenord:", fields[8],
       };
-      
-      
+
+
       for(int n = 0, i = 0; i < lis.length; i ++, n += 2){
-         if (lis[i] == false) 
+         if (lis[i] == false)
             message[n] = "<html><font color=#FF0000>" + message[n] + "</font>";
          else{
-            fields[i].setText(ans[i]); 
+            fields[i].setText(ans[i]);
          }
       }
-      
- 
+
+
       int option = JOptionPane.showConfirmDialog(null, message,"Skriv in dina uppgifer!", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
       if (option == JOptionPane.CLOSED_OPTION)
-          menu();  
+          menu();
       if (option == JOptionPane.CANCEL_OPTION){
          menu();
       }
       Boolean[] nli = new Boolean[9];
-      if (option == JOptionPane.OK_OPTION){  
+      if (option == JOptionPane.OK_OPTION){
          if (fields[0].getText() != null && fields[0].getText().length() == 12 && fields[0].getText().matches("[0-9]+")) {
             ans[0] = fields[0].getText();
             PNR = Long.parseLong(fields[0].getText());
             nli[0] = true;
-            
+
          }else{ans[0] = ""; nli[0] = false; checker = false;}
          if (fields[1].getText() != null) {
             ans[1] = fields[1].getText();
@@ -288,21 +261,21 @@ public class DBManager {
          }else{ans[8] = ""; nli[8] = false; checker = false;}
 
       }
-      
-     
+
+
       if (checker == false)
          generateMemDialog(nli, ans);
    }
-   
+
    public String[] generateLogInDialog(Boolean wrong, String[] result) {
-	   
-	   
+
+
 	   JTextField usernameField = new JTextField(result[0]);
 	   JPasswordField passwordField = new JPasswordField();
-	   
-	   
+
+
 	   Object[] options = {"Log In", "Tillbaka", "Bli Medlem"};
-	   
+
 	   Object[] message = {
 			   "Email: ", usernameField,
 			   "Lösenord: ", passwordField,
@@ -312,13 +285,13 @@ public class DBManager {
 			   "Email: ", usernameField,
 			   "Lösenord: ", passwordField,
 	   };
-	   
+
 	   if(!wrong) {
 		   int option = JOptionPane.showOptionDialog(null, message,"Skriv in dina uppgifer!", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options,options[0]);
 		     System.out.println("Option = " + option);
-		   	  if (option == JOptionPane.CLOSED_OPTION) 
+		   	  if (option == JOptionPane.CLOSED_OPTION)
 		    	  menu();
-		      else if (option == 1) 
+		      else if (option == 1)
 		    	  menu();
 		      else if (option == 0) {
 		    	  result[0] = usernameField.getText();
@@ -326,7 +299,7 @@ public class DBManager {
 		      }else if (option == 2){
 		    	  BliMedlem();
 		      }
-		   	  
+
 	   }else {
 		   int option = JOptionPane.showOptionDialog(null, messageWrong,"Skriv in dina uppgifer!", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options,options[0]);
 		      if (option == JOptionPane.CLOSED_OPTION) {
@@ -335,24 +308,24 @@ public class DBManager {
 		          menu(); System.out.print("close");}
 		      else if (option == 0) {
 		    	  result[0] = usernameField.getText();
-	    		  result[1] = String.valueOf(passwordField.getPassword());	    
+	    		  result[1] = String.valueOf(passwordField.getPassword());
 		      }else if (option == 2) {
 		    	  BliMedlem();
 		      }
-		      
+
 	   }
-	return result;  
-   }	   
-   
+	return result;
+   }
+
    public int getNumOfRows(String table) {
 	   String sql = "select count(*) from "+ table;
 	   int result = 0;
-	   
+
 	   try {
            Statement stmt  = conn.createStatement();
            ResultSet rs    = stmt.executeQuery(sql);
            result = rs.getInt(1);
-       
+
 	   }catch (SQLException e) {
 		   System.out.println("Funkar ej");
 		   System.out.println(e.getMessage());
@@ -360,11 +333,35 @@ public class DBManager {
 
 	   return result;
    }
-     
+
+   
+   public Boolean insertToDB(String table, Object[] values) {
+	   Boolean suc = false;
+	   String nuOfVal = "";
+	   
+	   for(int i = 1; i<values.length;i++) {
+		   nuOfVal += ",?";
+	   }
+	   
+	   String sql = "INSERT INTO " + table + " VALUES (?" + nuOfVal + ")";
+	   System.out.println(sql);
+	   
+	   try {
+           PreparedStatement pstmt = conn.prepareStatement(sql);
+           System.out.println("Funkar kung");
+
+           //Funkar allt ändrar jag frågetäcknena i sql-stringen(sql) till de som någon har matat in
+	       for(int i = 0; i<values.length;i++) {
+	    	   pstmt.setObject(i+1, values[i]);
+	       }
+           pstmt.executeUpdate();
+           //Det lyckades och då sätts stringen(result) som ska returnas till ett bekräftande
+           suc = true;
+     }
+     catch (SQLException e){
+           //Det misslyckades och då sätts stringen(result) till felmeddelandet som kommer från sql
+           System.out.println(e.toString());     
+     }
+	 return suc;
+   }
 }
-
-
-
-
-	
-
